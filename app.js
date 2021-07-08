d2093d9c5b9ef3fb0200816cbee7151cc574e5b1
_Allan <allankipchumba76@gmail.com>
2021-07-08 17:44:48 +0300

    level 7 - OAuth - sign in with google

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyPasrser = require("body-parser");
 const ejs = require("ejs");
@@ -6,16 +7,19 @@ const mongoose = require("mongoose");
 const session = require("express-session")
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose")
+    // require passport strategy
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(bodyPasrser.urlencoded({ extended: true }));
 
 // set the app to use sessions
 app.use(session({
-    secret: "Our little secret.",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
 }));
@@ -30,24 +34,59 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
-// set up userSchema to use passportLocalMongoose as a plugin
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("user", userSchema);
 
-// set up passport to creat a local login strategy and serialise and deserialize the user
+// set up passport to create a local login strategy
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+// serialise and deserialize the user
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
+// set up google strategy
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function(err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get("/auth/google",
+    // authenticate the user: google strategy
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect secrets page.
+        res.redirect("/secrets");
+    });
 
 app.get("/login", (req, res) => {
     res.render("login");
@@ -71,6 +110,9 @@ app.get("/logout", (req, res) => {
     res.redirect("/");
 });
 
+
+
+
 app.post("/register", (req, res) => {
 
     User.register({ username: req.body.username }, req.body.password, (err, user) => {
@@ -78,8 +120,8 @@ app.post("/register", (req, res) => {
             console.log(err);
             res.redirect("/register");
         } else {
-            // authenticate the user; local authentication
-            // set up cookie creating a log in sestion
+            // authenticate the user; local strategy
+            // set up cookie creating a log in session
             passport.authenticate("local")(req, res, () => {
                 res.redirect("/secrets");
             });
@@ -109,4 +151,4 @@ app.post("/login", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
-})
+});
